20d9f28c38f33f4e500bd762633abb6fd59338e5
corentin <hello@corentin-thomas.fr>
2023-12-02 09:19:58 +0100

    feat: init node+TS+eslint+prettier project

---
+++ .eslintrc.ts
@@ -0,0 +1,19 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/strict",
+    "plugin:prettier/recommended",
+    "prettier",
+    "eslint:recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    project: "tsconfig.json",
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+};
---
+++ .prettierrc.ts
@@ -0,0 +1,6 @@
+module.exports = {
+  semi: true,
+  arrowParens: "always",
+  trailingComma: "all",
+  singleQuote: true,
+};
